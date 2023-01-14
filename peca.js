7b3e7a9653ef9697e3685db6d2153cfc03e0155c
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-01-14 11:27:32 +0100

    elágazás a külön adattároláshoz

--- peca.js
+++ peca.js
@@ -1,7 +1,11 @@
 function ezustkaraszadat(){
-    var ezustkarasz_kg= parseInt(prompt("Hozzáadandó(kg):"));
-    var ezustkarasz_db= parseInt(prompt("Hozzáadandó(db):"));
-    var ezustkarasz_ki= document.getElementById("ezustkarasz_kg").textContent += ezustkarasz_kg;
+    var kivagy= prompt("Kinek írod az adatokat?")
+    if(kivagy=="Zsolti" || kivagy=="Zsüli") {
+        var ezustkarasz_kg= parseInt(prompt("Hozzáadandó(kg):"));
+        var ezustkarasz_db= parseInt(prompt("Hozzáadandó(db):"));
+        var ezustkarasz_ki= document.getElementById("ezustkarasz_kg").textContent += ezustkarasz_kg;
+    }
+    
     
 }
 

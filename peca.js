7f5cdc9ba7d6a04924a8bb54c9d030a5a6a38739
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-03-04 12:38:28 +0100

    oldal kartyakra szabasa

--- peca.js
+++ peca.js
@@ -1,13 +1,3 @@
-function ezustkaraszadat(){
-    var kivagy= prompt("Kinek írod az adatokat?")
-    if(kivagy=="Zsolti" || kivagy=="Zsüli") {
-        var ezustkarasz_kg= parseInt(prompt("Hozzáadandó(kg):"));
-        var ezustkarasz_db= parseInt(prompt("Hozzáadandó(db):"));
-        var ezustkarasz_ki= document.getElementById("ezustkarasz_kg").textContent += ezustkarasz_kg;
-    }
-    
-    
-}
 
 
 

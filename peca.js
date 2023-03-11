ebc45fb69e633e36648541f7fa8d8554cfcf2189
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-03-11 10:36:52 +0100

    zs_db szamolas

--- peca.js
+++ peca.js
@@ -1,3 +1,18 @@
+let ezust_karasz={
+    zs_db: Number(),
+    zs_kg: Number(),
+    d_db:Number(),
+    d_kg:Number()
+};
+function ezust_szamolas(){
+    
+    let adat= Number(document.getElementById("zs_ezust_db").innerHTML);
+    ezust_karasz.zs_db=adat;
+    let ujszam= parseInt(prompt("Kérlek add meg a darabszámot!"));
+    ezust_karasz.zs_db+= ujszam;
+    Number(document.getElementById("zs_ezust_db").innerHTML= `${ezust_karasz.zs_db} db`);
+    
+} 
 
 
 

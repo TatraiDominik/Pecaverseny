177761a4e2e2e6335765bcc758c905364a858ea1
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-03-11 10:55:24 +0100

    helyes szamolas zs_db

--- peca.js
+++ peca.js
@@ -1,16 +1,25 @@
 let ezust_karasz={
     zs_db: Number(),
-    zs_kg: Number(),
+    zs_kg:parseFloat(Number()) ,
     d_db:Number(),
-    d_kg:Number()
+    d_kg:parseFloat(Number())
 };
 function ezust_szamolas(){
     
-    let adat= Number(document.getElementById("zs_ezust_db").innerHTML);
-    ezust_karasz.zs_db=adat;
-    let ujszam= parseInt(prompt("Kérlek add meg a darabszámot!"));
-    ezust_karasz.zs_db+= ujszam;
-    Number(document.getElementById("zs_ezust_db").innerHTML= `${ezust_karasz.zs_db} db`);
+    let versenyzo= prompt("Kérlek add meg a neved kezdőbetűjét!");
+    if(versenyzo =="zs"){
+        let zs_db_adat= Number(document.getElementById("zs_ezust_db").innerHTML);
+        let zs_kg_adat=parseFloat(Number(document.getElementById("zs_ezust_kg").innerHTML)) ;
+        ezust_karasz.zs_db=zs_db_adat;
+        ezust_karasz.zs_kg=zs_kg_adat;
+        let zs_ujszam_db= parseInt(prompt("Kérlek add meg a darabszámot!"));
+        ezust_karasz.zs_db+= zs_ujszam_db;
+        let zs_ujszam_kg=parseFloat(Number(prompt("Kérlek add meg a fogott hal kg-ját!"))) ;
+        ezust_karasz.zs_kg+= zs_ujszam_kg;
+        Number(document.getElementById("zs_ezust_db").innerHTML= ezust_karasz.zs_db);
+        parseFloat(Number(document.getElementById("zs_ezust_kg").innerHTML= ezust_karasz.zs_kg));
+    }
+
     
 } 
 

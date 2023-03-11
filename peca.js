8fca06fd6d993d5b4a7dbf663d744e3a94ef199e
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-03-11 11:03:05 +0100

    ezustkarasz szamolas kesz

--- peca.js
+++ peca.js
@@ -18,6 +18,17 @@ function ezust_szamolas(){
         ezust_karasz.zs_kg+= zs_ujszam_kg;
         Number(document.getElementById("zs_ezust_db").innerHTML= ezust_karasz.zs_db);
         parseFloat(Number(document.getElementById("zs_ezust_kg").innerHTML= ezust_karasz.zs_kg));
+    }else{
+        let d_db_adat= Number(document.getElementById("d_ezust_db").innerHTML);
+        let d_kg_adat=parseFloat(Number(document.getElementById("d_ezust_kg").innerHTML)) ;
+        ezust_karasz.d_db=d_db_adat;
+        ezust_karasz.d_kg=d_kg_adat;
+        let d_ujszam_db= parseInt(prompt("Kérlek add meg a darabszámot!"));
+        ezust_karasz.d_db+= d_ujszam_db;
+        let d_ujszam_kg=parseFloat(Number(prompt("Kérlek add meg a fogott hal kg-ját!"))) ;
+        ezust_karasz.d_kg+= d_ujszam_kg;
+        Number(document.getElementById("d_ezust_db").innerHTML= ezust_karasz.d_db);
+        parseFloat(Number(document.getElementById("d_ezust_kg").innerHTML= ezust_karasz.d_kg));
     }
 
     

56d51e2be9a0f3a3f4fc4f9e12671332453f13f6
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-03-15 13:55:44 +0100

    suger, kosullo szamolas mentes

--- peca.js
+++ peca.js
@@ -91,10 +91,12 @@ function egyebkeszeg_szamolas(){
         egyebkeszegek.d_kg+= d_ujszam_kg;
         Number(document.getElementById("d_egyb_keszeg_db").innerHTML= egyebkeszegek.d_db);
         parseFloat(Number(document.getElementById("d_egyb_keszeg_kg").innerHTML= egyebkeszegek.d_kg));
+        //adatmentés START
         let ujddb=document.getElementById("d_egyb_keszeg_db").innerHTML;
         localStorage.setItem("e_d_db_input",ujddb);
         let ujdkg=document.getElementById("d_egyb_keszeg_kg").innerHTML;
         localStorage.setItem("e_d_kg_input",ujdkg);
+        //adatmentés END
     }
     else{
         window.alert("Nincs ilyen versenyző!");
@@ -143,10 +145,12 @@ function jaszkeszeg_szamolas(){
         jaszkeszeg.d_kg+= d_ujszam_kg;
         Number(document.getElementById("d_jasz_db").innerHTML= jaszkeszeg.d_db);
         parseFloat(Number(document.getElementById("d_jasz_kg").innerHTML= jaszkeszeg.d_kg));
+        //adatmentés START
         let ujddb=document.getElementById("d_jasz_db").innerHTML;
         localStorage.setItem("j_d_db_input",ujddb);
         let ujdkg=document.getElementById("d_jasz_kg").innerHTML;
         localStorage.setItem("j_d_kg_input",ujdkg);
+        //adatmentés END
     }
     else{
         window.alert("Nincs ilyen versenyző!");
@@ -194,17 +198,129 @@ function dever_szamolas(){
         dever.d_kg+= d_ujszam_kg;
         Number(document.getElementById("d_dever_db").innerHTML= dever.d_db);
         parseFloat(Number(document.getElementById("d_dever_kg").innerHTML= dever.d_kg));
+        //adatmentés START
         let ujddb=document.getElementById("d_dever_db").innerHTML;
         localStorage.setItem("d_d_db_input",ujddb);
         let ujdkg=document.getElementById("d_dever_kg").innerHTML;
         localStorage.setItem("d_d_kg_input",ujdkg);
+        //adatmentés END
     }
     else{
         window.alert("Nincs ilyen versenyző!");
     }
     
 }
-//DÉVÉR
+//DÉVÉR END
+
+//SÜGÉR START
+
+let suger={
+    zs_db: Number(),
+    zs_kg:parseFloat(Number()) ,
+    d_db:Number(),
+    d_kg:parseFloat(Number())
+};
+
+function suger_szamolas(){
+    
+    let versenyzo= prompt("Kérlek add meg a neved kezdőbetűjét!");
+    if(versenyzo =="zs" || versenyzo=="Zs"){
+        let zs_db_adat= Number(document.getElementById("zs_suger_db").innerHTML);
+        let zs_kg_adat=parseFloat(Number(document.getElementById("zs_suger_kg").innerHTML)) ;
+        suger.zs_db=zs_db_adat;
+        suger.zs_kg=zs_kg_adat;
+        let zs_ujszam_db= parseInt(prompt("Kérlek add meg a darabszámot!"));
+        suger.zs_db+= zs_ujszam_db;
+        let zs_ujszam_kg=parseFloat(Number(prompt("Kérlek add meg a fogott hal kg-ját!"))) ;
+        suger.zs_kg+= zs_ujszam_kg;
+        Number(document.getElementById("zs_suger_db").innerHTML= suger.zs_db);
+        parseFloat(Number(document.getElementById("zs_suger_kg").innerHTML= suger.zs_kg));
+        //adatmentés START
+        let ujzsdb=document.getElementById("zs_suger_db").innerHTML;
+        localStorage.setItem("s_zs_db_input",ujzsdb);
+        let ujzskg=document.getElementById("zs_suger_kg").innerHTML;
+        localStorage.setItem("s_zs_kg_input",ujzskg);
+        //adatmentes END
+    }else if(versenyzo =="d"||versenyzo =="D"){
+        let d_db_adat= Number(document.getElementById("d_suger_db").innerHTML);
+        let d_kg_adat=parseFloat(Number(document.getElementById("d_suger_kg").innerHTML)) ;
+        suger.d_db=d_db_adat;
+        suger.d_kg=d_kg_adat;
+        let d_ujszam_db= parseInt(prompt("Kérlek add meg a darabszámot!"));
+        suger.d_db+= d_ujszam_db;
+        let d_ujszam_kg=parseFloat(Number(prompt("Kérlek add meg a fogott hal kg-ját!"))) ;
+        suger.d_kg+= d_ujszam_kg;
+        Number(document.getElementById("d_suger_db").innerHTML= suger.d_db);
+        parseFloat(Number(document.getElementById("d_suger_kg").innerHTML= suger.d_kg));
+        //adatmentés START
+        let ujddb=document.getElementById("d_suger_db").innerHTML;
+        localStorage.setItem("s_d_db_input",ujddb);
+        let ujdkg=document.getElementById("d_suger_kg").innerHTML;
+        localStorage.setItem("s_d_kg_input",ujdkg);
+        //adatmentés END
+    }
+    else{
+        window.alert("Nincs ilyen versenyző!");
+    }
+    
+}
+
+//SÜGÉR END
+
+//KŐSÜLLŐ START
+
+let kosullo={
+    zs_db: Number(),
+    zs_kg:parseFloat(Number()) ,
+    d_db:Number(),
+    d_kg:parseFloat(Number())
+};
+
+function kosullo_szamolas(){
+    
+    let versenyzo= prompt("Kérlek add meg a neved kezdőbetűjét!");
+    if(versenyzo =="zs" || versenyzo=="Zs"){
+        let zs_db_adat= Number(document.getElementById("zs_kosullo_db").innerHTML);
+        let zs_kg_adat=parseFloat(Number(document.getElementById("zs_kosullo_kg").innerHTML)) ;
+        kosullo.zs_db=zs_db_adat;
+        kosullo.zs_kg=zs_kg_adat;
+        let zs_ujszam_db= parseInt(prompt("Kérlek add meg a darabszámot!"));
+        kosullo.zs_db+= zs_ujszam_db;
+        let zs_ujszam_kg=parseFloat(Number(prompt("Kérlek add meg a fogott hal kg-ját!"))) ;
+        kosullo.zs_kg+= zs_ujszam_kg;
+        Number(document.getElementById("zs_kosullo_db").innerHTML= kosullo.zs_db);
+        parseFloat(Number(document.getElementById("zs_kosullo_kg").innerHTML= kosullo.zs_kg));
+        //adatmentés START
+        let ujzsdb=document.getElementById("zs_kosullo_db").innerHTML;
+        localStorage.setItem("k_zs_db_input",ujzsdb);
+        let ujzskg=document.getElementById("zs_kosullo_kg").innerHTML;
+        localStorage.setItem("k_zs_kg_input",ujzskg);
+        //adatmentes END
+    }else if(versenyzo =="d"||versenyzo =="D"){
+        let d_db_adat= Number(document.getElementById("d_kosullo_db").innerHTML);
+        let d_kg_adat=parseFloat(Number(document.getElementById("d_kosullo_kg").innerHTML)) ;
+        kosullo.d_db=d_db_adat;
+        kosullo.d_kg=d_kg_adat;
+        let d_ujszam_db= parseInt(prompt("Kérlek add meg a darabszámot!"));
+        kosullo.d_db+= d_ujszam_db;
+        let d_ujszam_kg=parseFloat(Number(prompt("Kérlek add meg a fogott hal kg-ját!"))) ;
+        kosullo.d_kg+= d_ujszam_kg;
+        Number(document.getElementById("d_kosullo_db").innerHTML= kosullo.d_db);
+        parseFloat(Number(document.getElementById("d_kosullo_kg").innerHTML= kosullo.d_kg));
+        //adatmentés START
+        let ujddb=document.getElementById("d_kosullo_db").innerHTML;
+        localStorage.setItem("k_d_db_input",ujddb);
+        let ujdkg=document.getElementById("d_kosullo_kg").innerHTML;
+        localStorage.setItem("k_d_kg_input",ujdkg);
+        //adatmentés END
+    }
+    else{
+        window.alert("Nincs ilyen versenyző!");
+    }
+    
+}
+
+//KŐSÜLLŐ END
 
 
 
@@ -285,4 +401,44 @@ function adatmentes(){
         document.getElementById("d_dever_kg").innerHTML = d_d_kg_ki;
     }
     //DÉVÉRKESZEG END
+
+    //SÜGÉR START
+    let s_zs_db_ki = localStorage.getItem("s_zs_db_input");
+    if (s_zs_db_ki) {
+        document.getElementById("zs_suger_db").innerHTML = s_zs_db_ki;
+    }
+    let s_zs_kg_ki = localStorage.getItem("s_zs_kg_input");
+    if (s_zs_kg_ki) {
+        document.getElementById("zs_suger_kg").innerHTML = s_zs_kg_ki;
+    }
+    let s_d_db_ki = localStorage.getItem("s_d_db_input");
+    if (s_d_db_ki) {
+        document.getElementById("d_suger_db").innerHTML = s_d_db_ki;
+    }
+    let s_d_kg_ki = localStorage.getItem("s_d_kg_input");
+    if (s_d_kg_ki) {
+        document.getElementById("d_suger_kg").innerHTML = s_d_kg_ki;
+    }
+    //SÜGÉR END
+
+    //KOSULLO START
+
+    let k_zs_db_ki = localStorage.getItem("k_zs_db_input");
+    if (k_zs_db_ki) {
+        document.getElementById("zs_kosullo_db").innerHTML = k_zs_db_ki;
+    }
+    let k_zs_kg_ki = localStorage.getItem("k_zs_kg_input");
+    if (k_zs_kg_ki) {
+        document.getElementById("zs_kosullo_kg").innerHTML = k_zs_kg_ki;
+    }
+    let k_d_db_ki = localStorage.getItem("k_d_db_input");
+    if (k_d_db_ki) {
+        document.getElementById("d_kosullo_db").innerHTML = k_d_db_ki;
+    }
+    let k_d_kg_ki = localStorage.getItem("k_d_kg_input");
+    if (k_d_kg_ki) {
+        document.getElementById("d_kosullo_kg").innerHTML = k_d_kg_ki;
+    }
+
+    //KOSULLO END
 }
